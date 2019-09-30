e01e014064ef227c3ee611f97cffc38a8537aefc
knight124 <40920487+knight124@users.noreply.github.com>
2019-09-30 00:55:14 -0400

    - add multi image input functionality to convolution layers

--- image recognition.py
+++ image recognition.py
@@ -22,26 +22,41 @@ def loadData(address, numimages, sizeImages):
 
 class convolve:
 
-    def __init__(self, size, num_filters):
+    def __init__(self, size, num_filters, isinput):
         self.num_filters = num_filters
         self.size = size
+        self.isinput = isinput
         self.filters = np.random.rand(num_filters, size, size) / (size * size)
 
     def iteration_reg(self, image):
+
         width, height = image.shape
 
         for i in range(height - (self.size - 1)):
             for j in range(width - (self.size - 1)):
                 reg = image[i:(i + self.size), j:(j + self.size)]
+
                 yield reg, i, j
 
     def forward(self, input):
-        width, height = input.shape
-        out = np.zeros((height - (self.size - 1), width - (self.size - 1), self.num_filters))
+        if self.isinput:
+            width, height = input.shape
+            num_input = 1
+        else:
+            num_input, width, height = input.shape
+            self.filters = np.random.rand(self.num_filters * num_input, self.size, self.size) / (self.size * self.size)
 
-        for reg, i, j in self.iteration_reg(input):
+        out = np.zeros((self.num_filters * num_input, height - (self.size - 1), width - (self.size - 1)))
+        if self.isinput:
 
-            out[i, j] = np.sum(reg * self.filters, axis=(1, 2))
+            for reg, i, j in self.iteration_reg(input):
+                out[:, i, j] = np.sum(reg * self.filters, axis=(1, 2))
+        else:
+            for x in range(num_input):
+                for reg, i, j in self.iteration_reg(input[x, :, :]):
+                    out[x * self.num_filters:x * self.num_filters + self.num_filters, i, j] = np.sum(
+                        reg * self.filters[x * self.num_filters:x * self.num_filters + self.num_filters, :, :],
+                        axis=(1, 2))
 
         return out
 
@@ -54,19 +69,20 @@ class pool:
 
     def iterat(self, image):
         reg = 0
-        width, height,_ = image.shape
+        # print(image.shape)
+        _, width, height = image.shape
         for i in range(height - (self.size - 1)):
             for j in range(width - (self.size - 1)):
+                reg = np.amax(image[:, i:(i + self.size), j:(j + self.size)], axis=(1, 2))
 
-                reg =np.amax( np.array(image[i:(i + self.size), j:(j + self.size)]),axis=(1,2))
                 yield reg, i, j
 
     def pooling(self, input):
-        width, height,num_filters = input.shape
-        out = np.zeros((height - (self.size - 1), width - (self.size - 1), num_filters))
+        num_filters, width, height = input.shape
+        out = np.zeros((num_filters, height - (self.size - 1), width - (self.size - 1)))
 
         for reg, i, j in self.iterat(input):
-            out[i, j] = reg
+            out[:, i, j] = reg
 
         return out
 
@@ -79,16 +95,23 @@ if __name__ == "__main__":
 
     training_data = np.asarray(loadData(addresstraining, numimages, sizeimages))
 
-    hl1 = convolve(3, 8)
+    hl1 = convolve(3, 8, 1)
     pl1 = pool(5)
-    hl2 = convolve(3,16)
+    hl2 = convolve(3, 16, 0)
     pl2 = pool(3)
-    # hl3 =convolve(5,8)
-    # pl3 = pool(14,)
-    print(training_data.shape)
-    out = hl1.forward(training_data[0])
-    out = pl1.pooling(out)
-    print (out[:,:,0].shape)
-    out = hl2.forward(out[:,:,0])
+    hl3 = convolve(5, 8, 0)
 
+    out = hl1.forward(training_data[0])
+
+    out = pl1.pooling(out)
+    # print(out.shape)
+    out = hl2.forward(out)
     print(out.shape)
+    out = pl2.pooling(out)
+
+    out = hl3.forward(out)
+
+    pl3 = pool(out.shape[1])
+    out = pl3.pooling(out)
+    print(out.shape)
+
